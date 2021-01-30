9562004888cb2f27513bd6e95481e3b3f09c9378
CaladBlo <CaladBlogBaal@gmail.com>
2021-01-30 05:06:34 +0000

    added example config

---
+++ exampleconfig.py
@@ -0,0 +1,6 @@
+import os
+__bot_token__ = os.environ.get("BOT_TOKEN")
+__credentials__ = "postgres://postgres:DatabasePassword@HostIPAdress/DatabaseName"
+__fflogs_api_key__ = "api key here"
+__xivapikey__ = "api key here"
+__prefixes__ = ["stuff",  "blank ", "dab "]
